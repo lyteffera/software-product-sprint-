1badf477390f2f938bd8ac48efab14dd968de623
Leah Teffera <lteffera1@gmail.com>
2020-06-04 00:24:03 -0400

    Add image of dog to page that changes when a button is clicked. It cycles through 4 pictures in the images folder. Create structure for portfolio page, and bulleted lists describing technical and nontechnical interests. Add minor style elements to page.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,18 +11,22 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+var num = 0; 
 /**
- * Adds a random greeting to the page.
+ * Changes the image of the dog on the page. 
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+function changeImage(){
+  //var num = Math.floor(Math.random() * 4);
+  num++;
+  if (num >= 4){
+    num = 0;
+  }
+  const dog = 'images/dog' + num + '.jpg';
+  $('#dog_pic').attr('src', dog); 
+  console.log(num);
+  console.log(dog);
 }
+$('#get_dog').click(changeImage);
+
+
+
